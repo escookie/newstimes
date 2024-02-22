a13762c3b5273649cdef6da0aada681f4fb1d996
choisojin <choisojin@naver.com>
2024-02-22 17:05:19 +0900

    7th commit

--- main.js
+++ main.js
@@ -78,20 +78,28 @@ const getNewsByKeyword = async() =>{
 }
 
 const render=()=>{
-    const newsHTML = newsList.map(news=>`<div class="row news">
+    const newsHTML = newsList.map(news=>`
+    <div class="row news">
     <div class="col-lg-4">
         <img class="news-img-size" src=${news.urlToImage} onerror="imgError(this)">
     </div>
     <div class="col-lg-8 content">
         <h2>${news.title}</h2>
-        <p>
-            ${news.description}
-        </p>
+        <p>${
+            news.description == null || news.description == ""
+              ? "내용없음"
+              : news.description.length > 200
+              ? news.description.substring(0, 200) + "..."
+              : news.description
+          }</p>
+          <div>${news.source.name || "no source"}  ${moment(
+            news.publishedAt
+          ).fromNow()}</div>
         <div>
             ${news.source.name} * ${news.publishedAt}
         </div>
     </div>
-</div>`
+    </div>`
     )
     .join("");    
     document.getElementById('news-board').innerHTML = newsHTML;
